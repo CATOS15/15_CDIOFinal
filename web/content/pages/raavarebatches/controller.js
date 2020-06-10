f67d3aebf9cc3fa2d7cb3aadc34b6d5a3a349c32
tobias <tobiasobk@gmail.com>
2020-06-10 12:30:34 +0200

    produktbatches tilføjelse af tabel og form

--- web/content/pages/raavarebatches/controller.js
+++ web/content/pages/raavarebatches/controller.js
@@ -1,3 +1,9 @@
 angular.module('CDIOFinal').controller('raavarebatchesController', ['$scope', 'raavarebatchesModel', function ($scope, raavarebatchesModel) {
     $scope.raavarebatchesModel = raavarebatchesModel;
+
+
+
+    $scope.orderByMe = function(x) {
+        $scope.myOrderBy = x;
+    }
 }]);
--- web/content/pages/raavarebatches/model.js
+++ web/content/pages/raavarebatches/model.js
@@ -1,3 +1,14 @@
 angular.module('CDIOFinal').service('raavarebatchesModel', function () {
-    this.dummy = "dummy";
+    this.batches = [
+        {rbId:'1',raavareId:'1', maengde:'10,1234'},
+        {rbId:'2',raavareId:'2', maengde:'20,1234'},
+        {rbId:'3',raavareId:'3', maengde:'30,1234'},
+        {rbId:'4',raavareId:'4', maengde:'40,1234'},
+        {rbId:'5',raavareId:'1', maengde:'41,1234'},
+        {rbId:'6',raavareId:'2', maengde:'42,1234'},
+        {rbId:'7',raavareId:'3', maengde:'43,1234'},
+        {rbId:'8',raavareId:'4', maengde:'44,1234'},
+        {rbId:'9',raavareId:'4', maengde:'45,1234'}
+    ];
+
 });
