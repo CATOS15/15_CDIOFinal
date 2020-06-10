1a467caae2c3d18e2f0556adea42d28ab9dacf3a
chris <christian-frost@live.dk>
2020-06-10 15:32:36 +0200

    Råvare front-end

--- web/content/pages/raavare/controller.js
+++ web/content/pages/raavare/controller.js
@@ -1,3 +1,7 @@
 angular.module('CDIOFinal').controller('raavareController', ['$scope', 'raavareModel', function ($scope, raavareModel) {
     $scope.raavareModel = raavareModel;
+
+    $scope.orderByMe = function(x) {
+        $scope.myOrderBy = x;
+    }
 }]);
--- web/content/pages/raavare/model.js
+++ web/content/pages/raavare/model.js
@@ -1,3 +1,13 @@
 angular.module('CDIOFinal').service('raavareModel', function () {
-    this.dummy = "dummy";
+    this.raavare = [
+        {raavareId:'1',raavareName:'salt'},
+        {raavareId:'2',raavareName:'sukker'},
+        {raavareId:'3',raavareName:'citron'},
+        {raavareId:'4',raavareName:'mel'},
+        {raavareId:'5',raavareName:'saltsyre'},
+        {raavareId:'6',raavareName:'stoffer'},
+        {raavareId:'7',raavareName:'heroin'},
+        {raavareId:'8',raavareName:'gulerødder'}
+    ];
+
 });
