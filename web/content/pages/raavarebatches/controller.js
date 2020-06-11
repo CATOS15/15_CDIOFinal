16664e17bedf822de1efedebac911ba52c6d2b2e
tobias <tobiasobk@gmail.com>
2020-06-11 14:50:11 +0200

    accordian er glad igen ^^

--- web/content/pages/raavarebatches/controller.js
+++ web/content/pages/raavarebatches/controller.js
@@ -1,7 +1,7 @@
 angular.module('CDIOFinal').controller('raavarebatchesController', ['$scope', 'raavarebatchesModel', function ($scope, raavarebatchesModel) {
     $scope.raavarebatchesModel = raavarebatchesModel;
 
-
+    $scope.showAccordian = false;
 
     $scope.orderByMe = function(x) {
         $scope.myOrderBy = x;
