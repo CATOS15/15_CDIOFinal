560dec3d7a8a6fe45ad2cacecf0d65ea94a1b4b5
chris <christian-frost@live.dk>
2020-06-12 13:07:59 +0200

    RaavareBatchDAO frontEnd stylet

--- web/content/pages/raavarebatches/controller.js
+++ web/content/pages/raavarebatches/controller.js
@@ -11,7 +11,20 @@ angular.module('CDIOFinal').controller('raavarebatchesController', ['$scope', 'r
         raavarebatchesService.getRaavareBatches();
         raavareService.getRaavarer();
     };
-
+    $scope.toggleItem = function(raavareBatch){
+        $scope.newItem = false;
+        if(raavarebatchesModel.raavareBatch === raavareBatch){
+            raavarebatchesModel.raavareBatch = null;
+        }
+        else{
+            raavarebatchesModel.raavareBatch = raavareBatch;
+        }
+    };
+    $scope.toggleNewItem = function(){
+        if($scope.newItem) return;
+        $scope.newItem = true;
+        raavarebatchesModel.raavareBatch = {rbId: "", raavareId: raavareModel.raavarer[0].raavareId, maengde: "", leverandoer: ""};
+    };
     $scope.save = function(){
         raavarebatchesService.createRavareBatch(raavarebatchesModel.raavareBatch);
     };
