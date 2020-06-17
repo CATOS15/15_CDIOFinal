23ce5438558a9e5ff0010b85d009ade4b3aa51e3
creoludifico <omoeltoft@gmail.com>
2020-06-17 15:50:12 +0200

    komma skal ikke være tilladt

--- web/content/global/CDIOFinal/controller.js
+++ web/content/global/CDIOFinal/controller.js
@@ -1,6 +1,24 @@
 ﻿angular.module('CDIOFinal').controller('CDIOFinalController', ['$rootScope', '$scope', 'CDIOFinalModel', '$location', 'CDIOFinalService', function ($rootScope, $scope, CDIOFinalModel, $location, CDIOFinalService) {
     $scope.CDIOFinalModel = CDIOFinalModel;
 
+    $scope.onlyNumber = function(model){
+        try{
+            var newModel = model.toString().replace(",", "");
+            return parseFloat(newModel.replace(".", ""));
+        }
+        catch(ex){
+            return model;
+        }
+    };
+    $scope.changeCommaToDot = function(model){
+        try{
+            return parseFloat(model.toString().replace(",", ""));
+        }
+        catch(ex){
+            return model;
+        }
+    };
+
     $scope.init = function(){
         $scope.checkLocalStorageToken();
         CDIOFinalService.getLogin();
