75a797a2f869fb393e7518fd3675ee109472d164
chris <christian-frost@live.dk>
2020-06-17 16:30:43 +0200

    Afvejning ikke låser ved error gem - rettet

--- web/content/pages/afvejning/model.js
+++ web/content/pages/afvejning/model.js
@@ -1,6 +1,7 @@
 angular.module('CDIOFinal').service('afvejningModel', function () {
     this.userProduktBatch = null;
     this.userProduktBatches = [];
+    this.newItem = false;
 
     this.msg = "";
     this.error = "";
--- web/content/pages/afvejning/service.js
+++ web/content/pages/afvejning/service.js
@@ -1,4 +1,6 @@
 angular.module('CDIOFinal').service('afvejningService', ['$http', 'afvejningModel','CDIOFinalModel', 'produktionsbatchService', function ($http, afvejningModel,CDIOFinalModel, produktionsbatchService) {
+    var _this = this;
+
     this.getAfvejninger = function(){
         afvejningModel.error = "";
         afvejningModel.msg = "";
@@ -7,16 +9,19 @@ angular.module('CDIOFinal').service('afvejningService', ['$http', 'afvejningMode
             url: CDIOFinalModel.apiURL + "afvejning"
         }).then(function (resp) {
             afvejningModel.userProduktBatches = resp.data;
+            _this.resetItem();
         }, function (errorResp) {
             afvejningModel.error = errorResp.data;
         });
     };
 
     this.createAfvejning = function(userProduktBatch){
+        var newItems = [];
         afvejningModel.error = "";
         afvejningModel.msg = "";
         for(var i = 0;i<userProduktBatch.afvejninger.length;i++){
             if(userProduktBatch.afvejninger[i].newItem){
+                newItems.push(i);
                 delete(userProduktBatch.afvejninger[i].newItem)
             }
         }
@@ -28,8 +33,18 @@ angular.module('CDIOFinal').service('afvejningService', ['$http', 'afvejningMode
             afvejningModel.msg = "Afvejning " + userProduktBatch.rbId + " oprettet";
             produktionsbatchService.getProduktBatches();
             afvejningModel.userProduktBatches.push(resp.data);
+            _this.resetItem();
         }, function (errorResp) {
             afvejningModel.error = errorResp.data;
+            newItems.forEach(function (item) {
+                userProduktBatch.afvejninger[item].newItem = true;
+            });
         });
+    };
+
+
+    this.resetItem = function(){
+        afvejningModel.newItem = false;
+        afvejningModel.userProduktBatch = null;
     };
 }]);
