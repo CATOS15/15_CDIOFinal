215a2a4fcdbbb98c3873b469f94bfaa711d9c875
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2020-06-19 17:50:09 +0200

    små rettelser

--- web/content/pages/login/controller.js
+++ web/content/pages/login/controller.js
@@ -1,9 +1,8 @@
-angular.module('CDIOFinal').controller('loginController', ['$scope', 'loginModel', 'loginService', function ($scope, loginModel, loginService) {
+angular.module('CDIOFinal').controller('loginController',
+    ['$scope', 'loginModel', 'loginService',
+    function ($scope, loginModel, loginService) {
     $scope.loginModel = loginModel;
     $scope.login = function(){
         loginService.login($scope.loginModel.loginUser);
-    };
-    $scope.createUser = function(){
-        loginService.createUser($scope.loginModel.newUser);
     };
 }]);
